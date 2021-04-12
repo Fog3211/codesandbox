f89d4e84f4476ce5eda18b0b4252cd04e04d75c2
Fog3211 <fog3211@163.com>
2021-04-12 16:36:11 +0800

    feat: add reproduce step

--- sources/formily-formlist-reset/src/App.tsx
+++ sources/formily-formlist-reset/src/App.tsx
@@ -1,74 +1,71 @@
 import React from "react";
-import {
-  Form,
-  FieldList,
-  FormItem,
-  createAsyncFormActions
-} from "@formily/antd";
-import { Button, Input, Space } from "antd";
-import type { IFormAsyncActions } from "@formily/antd";
-
-const actions: IFormAsyncActions = createAsyncFormActions();
+import { Button, Input, Space, Form, Divider } from "antd";
 
 type AnyObj = Record<string, any>;
 const FormGroup = () => {
+  const [form] = Form.useForm();
   return (
-    <Form actions={actions} style={{ width: 600 }}>
-      <FieldList initialValue={[]} name="lists1">
-        {({ state: state1, mutators: { push: onAdd1 } }) => (
+    <Form form={form} style={{ width: 600 }}>
+      <Form.List name={"lists1"} initialValue={[]}>
+        {(fields1, { add: onAdd1 }) => (
           <div style={{ marginTop: 30, marginLeft: 30 }}>
-            {state1.value.map((item1: AnyObj, index1: number) => (
-              <FieldList
-                initialValue={[]}
-                name={`lists1.${index1}.list2`}
-                key={index1}
-              >
-                {({ state: state2, mutators: { push: onAdd2 } }) => (
-                  <div style={{ marginTop: 30, marginLeft: 30 }}>
-                    {state2.value.map((item2: AnyObj, index2: number) => (
-                      <React.Fragment key={index2}>
-                        <FormItem
-                          label={`${index1}-${index2}`}
-                          name={`lists1.${index1}.list2.${index2}.field`}
-                          component={Input}
-                          style={{ width: 300 }}
-                        />
-                      </React.Fragment>
-                    ))}
-                    <Button
-                      onClick={() =>
-                        onAdd2({ field: `${index1}-${state2.value.length}` })
-                      }
-                      style={{ marginTop: 10 }}
-                    >
-                      Add2
-                    </Button>
-                  </div>
-                )}
-              </FieldList>
+            {fields1.map((item1: AnyObj, index1: number) => (
+              <div key={index1}>
+                <Form.List
+                  name={[index1, "list2"]}
+                  key={index1}
+                  initialValue={[]}
+                >
+                  {(fields2, { add: onAdd2 }) => (
+                    <div style={{ marginTop: 30, marginLeft: 30 }}>
+                      {fields2.map((item2: AnyObj, index2: number) => (
+                        <React.Fragment key={index2}>
+                          <Form.Item
+                            label={`${index1}-${index2}`}
+                            name={[index2, "field"]}
+                            initialValue="无"
+                          >
+                            <Input style={{ width: 300 }} />
+                          </Form.Item>
+                        </React.Fragment>
+                      ))}
+                      <Button
+                        onClick={() =>
+                          onAdd2({ field: `${index1}-${fields2.length}` })
+                        }
+                        style={{ marginTop: 10 }}
+                      >
+                        Add2
+                      </Button>
+                    </div>
+                  )}
+                </Form.List>
+                <Divider />
+              </div>
             ))}
-            <Button block onClick={() => onAdd1()} style={{ marginTop: 10 }}>
+            <Button
+              block
+              onClick={() => onAdd1()}
+              style={{ marginTop: 10 }}
+            >
               Add1
             </Button>
           </div>
         )}
-      </FieldList>
+      </Form.List>
       <br />
       <Space size={20}>
         <Button
           type="primary"
           onClick={() => {
-            actions.submit((values) => {
-              console.log(values);
-            });
+            form.validateFields().then(console.log);
           }}
         >
           Submit
         </Button>
         <Button
           onClick={() => {
-            actions.reset({ forceClear: true });
-            // actions.reset({ forceClear: true, clearInitialValue: true })
+            form.resetFields();
           }}
         >
           Reset
